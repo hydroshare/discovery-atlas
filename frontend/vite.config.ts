2b8329295bfc1edb569842b2837030b7d9b19bb8
Devin Cowan <dcowan@cuahsi.org>
2025-09-22 23:11:32 -0400

    update default base path in Vite config to '/search/'

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
     root: "./",
-    base: env.VITE_APP_BASE || "/",
+    base: env.VITE_APP_BASE || "/search/",
     envDir: "../",
     resolve: {
       alias: {
